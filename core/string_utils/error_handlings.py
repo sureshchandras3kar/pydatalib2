531d382c3c43ca840e255b2b68da8cab187833f9
sureshchandras3kar <sureshkavitha1997@gmail.com>
2023-12-14 16:00:38 +0530

    Task : string utils string_operations

---
+++ core/string_utils/error_handlings.py
@@ -0,0 +1,44 @@
+from typing import Any
+
+class PatternTypeNotFoundError(Exception):
+    """
+    Exception raised when a pattern type is not found.
+    """
+
+    def __init__(self, message="Pattern type not found."):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
+
+
+class InvalidPatternTypeError(Exception):
+    """
+    Exception raised when a pattern type is invalid
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def __str__(self):
+        return "Pattern type is invalid."
+
+
+class InvalidInputError(TypeError):
+    """
+    Custom error raised when received object is not a string as expected.
+    """
+
+    def __init__(self, input_data: Any) -> None:
+        """
+        Initialize the InvalidInputError instance.
+
+        :param input_data: Any received object
+        """
+        if input_data is None:
+            msg = 'No input provided'
+        else:
+            type_name = input_data.__class__.__name__
+            msg = f'Expected "str", received "{type_name}"'
+        super().__init__(msg)
---
+++ core/string_utils/regex_strings.py
@@ -0,0 +1,42 @@
+from enum import Enum
+from re import Pattern
+from re import compile
+from typing import Dict
+from .error_handlings import PatternTypeNotFoundError
+
+
+class PatternType(Enum):
+    IPV4 = "ipv4"
+    IPV6 = "ipv6"
+    EMAIL = "email"
+    PHONE_NUMBER = "phone_number"
+    URL = "url"
+
+
+class RePatterns:
+    IPV4_PATTERN: Pattern[
+        str] = r'^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
+    IPV6_PATTERN: Pattern[
+        str] = r'^[a-fA-F0-9]{1,4}(:[a-fA-F0-9]{1,4}){7}$'  # Regular expression to match an IPv6 address
+    EMAIL_PATTERN: Pattern[
+        str] = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'  # Regular expression to match an email address
+    PHONE_NUMBER_PATTERN: Pattern[str] = r'^\+?[1-9]\d{1,14}$'
+    URL_PATTERN: Pattern[str] = r'^(https?|ftp)://[^\s/$.?#][a-zA-Z0-9_-]*(\.[a-zA-Z0-9_-]+)+(/[^\s]*)?$'
+
+    def __init__(self):
+        self.PATTERN_MAPPING: Dict[PatternType, Pattern] = {
+            PatternType.IPV4: compile(self.IPV4_PATTERN),
+            PatternType.EMAIL: compile(self.EMAIL_PATTERN),
+            PatternType.PHONE_NUMBER: compile(self.PHONE_NUMBER_PATTERN),
+            PatternType.URL: compile(self.URL_PATTERN)
+        }
+
+    @property
+    def pattern(self) -> Dict[PatternType, Pattern]:
+        return self.PATTERN_MAPPING
+
+    def get_pattern(self, pattern_type: PatternType) -> Pattern:
+        pattern = self.pattern.get(pattern_type)
+        if pattern is None:
+            raise PatternTypeNotFoundError(f"Pattern type '{pattern_type}' not found.")
+        return pattern
